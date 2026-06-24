db44f1ba7f00623658fce73c8f0e9f77149c5817
Vedant Baranwal <vedantbaranwal1@gmail.com>
2026-06-24 17:34:27 +0530

    Update frontend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+const API = axios.create({
+  baseURL: "https://ai-interview-prep-6h9v.onrender.com",
 });
 
 export default api;
