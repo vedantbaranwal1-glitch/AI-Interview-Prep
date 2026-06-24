27cb68cc24eb0b650f82468448c83c763f0a4f5e
Vedant Baranwal <vedantbaranwal1@gmail.com>
2026-06-24 17:22:45 +0530

    Use DATABASE_URL from Render

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,11 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:admin123@localhost:5432/ai_interview_db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
